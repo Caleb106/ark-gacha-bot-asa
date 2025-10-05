4dcbfef6f1d30cfa37f9fdfead1882465d192923
AZX <125766660+AZX-215@users.noreply.github.com>
2025-10-05 04:06:38 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,41 @@
+screen_resolution: int = None # No longer in use. Just here cause people are thoughtless.
+base_path: str = None # No longer in use. Just here cause people are thoughtless.
+lag_offset: float = 1.3
+iguanadon: str = "GACHAIGUANADON"
+open_crystals: str = "GACHACRYSOPEN"
+collection_depo: str = "PHOENIXSNAILDEPO"
+drop_off: str = "GACHADEDI"
+bed_spawn: str = "GRAVITYRENDER"
+berry_station: str = "GACHABERRYSTATION"
+grindables: str = "GACHAGRINDABLES"
+berry_type: str = "berry"
+station_yaw: float = -141.27
+render_pushout: float = 170.54
+external_berry: bool = False
+height_ele: int = 3 
+height_grind: int = 3
+command_prefix: str = "%"
+singleplayer: bool = False
+server_number: str = 9306
+crafting: bool = False
+seeds_230: bool = False
+render_only: bool = True     # True = only render tasks
+enable_gacha: bool = False
+enable_pego:  bool = False
+enable_render: bool = True
+render_dwell_seconds: int = 25        # seconds per stop
+render_rest_seconds: int = 2700       # 45 minutes
+queue_post_interval: int = 60
+
+# YOUR discord channel IDs and bot API key. To find channel IDs enable developer mode in discord and right click the channel to copy ID.
+log_channel_gacha: int = 1424234467379384380
+log_active_queue: int = 1424234214433489027
+log_wait_queue: int = 1424234297358811137
+discord_api_key: str = ""
+
+
+if __name__ =="__main__":
+    
+    pass
+
+
