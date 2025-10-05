de4aba14f9cc5d03e99ac229261d5414b405a418
AZX <125766660+AZX-215@users.noreply.github.com>
2025-10-05 05:33:53 -0400

    Update compat_input.py

--- compat_input.py
+++ compat_input.py
@@ -1,8 +1,10 @@
-# compat_input.py
-try:
+import sys
+if sys.version_info >= (3, 13):
     import pydirectinput as pyautogui
-except Exception:
-    import pyautogui
-
+else:
+    try:
+        import pydirectinput as pyautogui
+    except ImportError:
+        import pyautogui
 pyautogui.PAUSE = 0.0
 pyautogui.FAILSAFE = False
