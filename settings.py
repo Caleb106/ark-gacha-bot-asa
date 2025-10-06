7072fcf2263ddec69386f4aa3ec1a6219e89a470
AZX <125766660+AZX-215@users.noreply.github.com>
2025-10-05 20:02:47 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ iguanadon: str = "GACHAIGUANADON"
 open_crystals: str = "GACHACRYSOPEN"
 collection_depo: str = "PHOENIXSNAILDEPO"
 drop_off: str = "GACHADEDI"
-bed_spawn: str = "GRAVITYRENDER"
+bed_spawn: str = "ALTRENDER"
+render_bed_spawn: str = "GRAVITYRENDER"
 berry_station: str = "GACHABERRYSTATION"
 grindables: str = "GACHAGRINDABLES"
 berry_type: str = "berry"
@@ -28,14 +29,13 @@ render_rest_seconds: int = 2700       # 45 minutes
 queue_post_interval: int = 60
 
 # YOUR discord channel IDs and bot API key. To find channel IDs enable developer mode in discord and right click the channel to copy ID.
-log_channel_gacha: int = 1424234467379384380
-log_active_queue: int = 1424234214433489027
-log_wait_queue: int = 1424234297358811137
+log_channel_gacha: int = 123
+log_active_queue: int = 123
+log_wait_queue: int = 123
 discord_api_key: str = ""
 
 
 if __name__ =="__main__":
     
     pass
-
 
